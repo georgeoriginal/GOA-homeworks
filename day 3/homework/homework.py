719894825d0613bd2588f9642ff88ffd6d1df004
georgeoriginal <georgetony1202@gmail.com>
2024-10-12 10:44:02 +0400

    third commit

---
+++ day 3/homework/homework.py
@@ -0,0 +1,69 @@
+name="giorgi"
+surname="katsitadze"
+print(name)
+print(surname)
+#me chemi saxeli printis daxmarebit da cvladebis, gavushvi chemi name da surname romlebsac davarqvi giorgi da katsitadze samkutxedze daweris
+#  shemdeg terminalshi machvena chemi saxeli da gvari
+my_information="i am a male, future man. i want to be a footballer"
+my_information1="i workout almost every day and i hate school"
+print(my_information + " " + my_information1)
+#cvladebs vqmnit tolobis nishnit  da misi danishnuleba aris erti sityvis daprintvit tu miveci cvladi
+#  meore mnishvneloba is gvichvenos terminalshi
+from turtle import *
+width(4)
+begin_fill()
+color("grey")
+circle(100)
+end_fill()
+penup()
+goto(-15,200)
+begin_fill()
+left(90)
+forward(40)
+right(90)
+forward(35)
+right(90)
+forward(42)
+end_fill()
+color("black")
+penup()
+goto(5,115)
+pendown()
+left(30)
+forward(70)
+penup()
+goto(-10,130)
+pendown()
+right(30)
+forward(65)
+penup()
+goto(-19,142)
+pendown()
+right(35)
+forward(55)
+penup()
+goto(-15,-100)
+right(220)
+pendown()
+shapesize(30)
+
+write("fasanauruli xinkali")
+
+
+
+
+
+exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+
+""
